590de5ccdf77515bd3837fff158994fcbe03b04c
Raushanray <raushankuray1998@gmail.com>
2023-04-11 10:09:22 +0530

    the 3rd way for implementing functional interface is most usefully the lambda expression  //3. using Lambda Expression

--- src/work/Main.java
+++ src/work/Main.java
@@ -5,22 +5,39 @@ public class Main {
         System.out.println("My System Starts !!");
 //        MyInterImpl myInter = new MyInterImpl();
 //        myInter.seyHello();
-        MyInter i = new MyInter() {
-            //this is an anonymous class
-            @Override
-            public void seyHello() {
-                System.out.println("this my fist anonymous class !!");
-            }
+//        MyInter i = new MyInter() {
+//            //this is an anonymous class
+//            @Override
+//            public void seyHello() {
+//                System.out.println("this my fist anonymous class !!");
+//            }
+//        };
+//        i.seyHello();
+//        MyInter i2 = new MyInter() {
+//            //this is an anonymous class
+//            @Override
+//            public void seyHello() {
+//                System.out.println("this my second anonymous class !!");
+//            }
+//        };
+//        i2.seyHello();
+
+        //using our interface with the help of lambda
+        MyInter i = () -> {
+            System.out.println("this is first time i am using lambda");
         };
         i.seyHello();
-        MyInter i2 = new MyInter() {
-            //this is an anonymous class
-            @Override
-            public void seyHello() {
-                System.out.println("this my second anonymous class !!");
-            }
-        };
+        //apply the lambda rule
+        /*If the body of the lambda expression contains only
+         one statement then the curly braces are optional. */
+
+        MyInter i1 = () -> System.out.println("this is first time i am using lambda with rule");
+        i1.seyHello();
+
+        //2nd time create
+        MyInter i2 = () -> System.out.println("this is second time i am using lambda with rule");
         i2.seyHello();
+
 
     }
 }
--- src/work/MyInter.java
+++ src/work/MyInter.java
@@ -9,6 +9,7 @@ public interface MyInter {
     //there are n number of way to use this interface
     //1. create separate  class and implements this interface..
     //2. anonymous class for implementing interface
+    //3. using Lambda Expression
 
 
 }
