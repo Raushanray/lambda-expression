73e788759aceb852369e96ab89cad393645d5712
Raushanray <raushankuray1998@gmail.com>
2023-04-11 10:27:35 +0530

    create one interface name as SumInter and add one abstract and this also a functional interface because here only one method,  now implements this method using lambda expression both type without using lamda rule and using lambda rule

--- src/work/Main.java
+++ src/work/Main.java
@@ -23,20 +23,38 @@ public class Main {
 //        i2.seyHello();
 
         //using our interface with the help of lambda
-        MyInter i = () -> {
-            System.out.println("this is first time i am using lambda");
-        };
-        i.seyHello();
+//        MyInter i = () -> {
+//            System.out.println("this is first time i am using lambda");
+//        };
+//        i.seyHello();
         //apply the lambda rule
         /*If the body of the lambda expression contains only
          one statement then the curly braces are optional. */
 
-        MyInter i1 = () -> System.out.println("this is first time i am using lambda with rule");
-        i1.seyHello();
+//        MyInter i1 = () -> System.out.println("this is first time i am using lambda with rule");
+//        i1.seyHello();
+//
+//        //2nd time create
+//        MyInter i2 = () -> System.out.println("this is second time i am using lambda with rule");
+//        i2.seyHello();
 
-        //2nd time create
-        MyInter i2 = () -> System.out.println("this is second time i am using lambda with rule");
-        i2.seyHello();
+        //here use SumInter interface
+        SumInter sumInter = (int a, int b) -> {
+            return a + b;
+        };
+        System.out.println(sumInter.sum(2, 6));
+        System.out.println(sumInter.sum(5, 8));
+
+        //apply the lambda rule
+        /*If the body of the lambda expression contains only
+         one statement then the curly braces are optional. */
+        /* Java compiler also inter the type of variable
+        passed in arguments, hence type is optional.*/
+        SumInter sumInter1 = (a, b) -> a + b;
+        System.out.println("run lambda with rule");
+
+        System.out.println(sumInter1.sum(5, 6));
+        System.out.println(sumInter1.sum(5, 9));
 
 
     }
